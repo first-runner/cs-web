f81a5bb7ea0b1182987055b9f294f08765a3d73a
Ivanov Aleksei Olegovich <iao675@bk.ru>
2024-11-07 05:16:30 +0300

    Закрытие приложения через 10с.

--- cs-web/Program.cs
+++ cs-web/Program.cs
@@ -1,25 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+//var builder = WebApplication.CreateBuilder(args);
+//// Add services to the container.
+//builder.Services.AddRazorPages();
+//var app = builder.Build();
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+//app.UseRouting();
+//app.UseAuthorization();
+//app.MapRazorPages();
+//app.Run();
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
+WebApplicationBuilder builder = WebApplication.CreateBuilder();
+WebApplication app = builder.Build();
+app.MapGet("/", () => "Hello World!");
+//app.Run();
+await app.StartAsync();
+await Task.Delay(10000);
+await app.StopAsync();
